daacb55f7b7dbe0c9928b3189c9f6800500a14f9
Daniel Edwards <dedwards0987654321@gmail.com>
2022-12-11 23:41:30 -0600

    wow hosting

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import json
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+
+async def root():
+
+    return {"message": "ZORBMOJI TAKEOVER"}
+
+
+@app.get("/json/{file_name}")
+def serve_json_file(file_name: str):
+    with open(f"static/{file_name}", "r") as f:
+        data=json.load(f)
+        return data
